df1f85986f68f04215f0197743d1061c28f52848
patter59 <34488830+patter59@users.noreply.github.com>
2018-10-17 17:11:23 -0400

    Add files via upload

---
+++ RomanNumeralConverter/RomanNumeralConverter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RomanNumeralConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtArabic = new System.Windows.Forms.TextBox();
+            this.txtRoman = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(418, 54);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtArabic
+            // 
+            this.txtArabic.Location = new System.Drawing.Point(125, 16);
+            this.txtArabic.Name = "txtArabic";
+            this.txtArabic.Size = new System.Drawing.Size(100, 20);
+            this.txtArabic.TabIndex = 1;
+            // 
+            // txtRoman
+            // 
+            this.txtRoman.Location = new System.Drawing.Point(393, 16);
+            this.txtRoman.Name = "txtRoman";
+            this.txtRoman.Size = new System.Drawing.Size(100, 20);
+            this.txtRoman.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Arabic Numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Roman Numerals ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(23, 79);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(0, 13);
+            this.lblWarning.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 105);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRoman);
+            this.Controls.Add(this.txtArabic);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtArabic;
+        private System.Windows.Forms.TextBox txtRoman;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ RomanNumeralConverter/RomanNumeralConverter/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomanNumeralConverter
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            ConversionSwitch();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtArabic.Text = "";
+            txtRoman.Text = "";
+            lblWarning.Text = "";
+        }
+
+        private void ConversionSwitch()
+        {
+            if(isRoman() == isArabic())
+            {
+                lblWarning.Text = "You either entered values in both fields or none at all please correct this issue and try again.";
+            }
+            if (isArabic() && !isRoman())
+            {
+                txtRoman.Text = arabicToRoman(int.Parse(txtArabic.Text));
+            }
+            if(isRoman() && !isArabic())
+            {
+                romanToArabic(txtRoman.Text.ToString().ToUpper());
+            }
+           
+        }
+        private bool isArabic()
+        {
+            if (txtArabic.Text != null && txtArabic.Text != "")
+            {
+                return isValidArabic();
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        private bool isValidArabic()
+        {
+            try
+            {
+                var arabic = int.Parse(txtArabic.Text);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public string arabicToRoman(int input)
+        {
+            var romanNumerals = new string[][]
+            {
+            new string[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            new string[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            new string[]{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            new string[]{"", "M", "MM", "MMM"}
+            };
+
+            var intArray = input.ToString().Reverse().ToArray();
+            var len = intArray.Length;
+            var romanNumeral = "";
+            var i = len;
+
+            while (i-- > 0)
+            {
+                romanNumeral += romanNumerals[i][int.Parse(intArray[i].ToString())];
+            }
+
+            return romanNumeral;
+        }
+
+        private bool isRoman()
+        {
+            if(txtRoman.Text != null && txtRoman.Text != "")
+            {
+                return isValidRoman();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool isValidRoman()
+        {
+            var romanInput = txtRoman.Text.ToString().ToUpper();
+
+            if(ruleCheck_ValidLetters(romanInput))
+            {
+                if(ruleCheck_RuleOfThree_IXCM(romanInput))
+                {
+                    return true;
+                }
+                else
+                {
+                    lblWarning.Text = "The Roman Numeral entered is not valid it violates the rule of three.";
+                    return false;
+                }
+            }
+            else
+            {
+                lblWarning.Text = "The Roman numeral entered is not valid try again.";
+                return false;
+            }
+
+        }
+        //The Romans wrote their numbers using letters; specifically the letters 'I' meaning '1', 
+        //'V' meaning '5', 'X' meaning '10', 'L' meaning '50', 'C' meaning '100', 'D' meaning '500', and 'M' meaning '1000'. 
+        private bool ruleCheck_ValidLetters(string input)
+        {
+            var allowableletters = "IVXLCDM";
+
+            foreach (char c in input)
+            {
+                if(!allowableletters.Contains(c.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row
+        private bool ruleCheck_RuleOfThree_IXCM(string input)
+        {
+            var l1 = "";
+            var l2 = "";
+            var l3 = "";
+            var l4 = "";
+
+            var charArray = input.ToArray();
+
+            for(int x = 3; x < charArray.Length; x++)
+            {
+                l1 = charArray[x - 3].ToString();
+                l2 = charArray[x - 2].ToString();
+                l3 = charArray[x - 1].ToString();
+                l4 = charArray[x].ToString();
+                if(l1 == l2 && l2 == l3 && l3 == l4)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        //The symbols 'V', 'L', and 'D' can never be repeated.
+        private bool ruleCheck_RepeatRule_VLD(string input)
+        {
+            var V = 0;
+            var L = 0;
+            var D = 0;
+
+            foreach(char c in input)
+            {
+                if (c.ToString() == "V") V++;
+                if (c.ToString() == "L") L++;
+                if (c.ToString() == "D") D++;
+
+                if (V > 1 || L > 1 || D > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        private void romanToArabic(string input)
+        {
+            var result = 0;
+
+            foreach (var c in input)
+            {
+                result += convertRomanToArabic(c);
+            }
+            if (input.Contains("IV") || input.Contains("IX"))
+                result -= 2;
+
+            if (input.Contains("XL") || input.Contains("XC"))
+                result -= 20;
+
+            if (input.Contains("CD") || input.Contains("CM"))
+                result -= 200;
+
+            txtArabic.Text = result.ToString();
+        }
+        private int convertRomanToArabic(char input)
+        {
+            switch(input)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: throw new ArgumentException("Invalid Format");
+            }
+        }
+
+
+    }
+}
